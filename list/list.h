71e42631c45848fbf024be94fd5b4ecb73b2dcdb
Merlon crakehall Bartoletti <gerus66@gmail.com>
2019-04-12 16:52:43 +0300

    add ft_lstcopy function - copy of list

--- list/list.h
+++ list/list.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 13:51:55 by mbartole          #+#    #+#             */
-/*   Updated: 2019/03/29 15:34:56 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/04/12 16:15:31 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ typedef struct	s_list
 
 t_list			*ft_lstnew(void const *content, size_t content_size);
 
-/*
+/* 
 ** just hook new element on the top of list (or push it to the back)
 */
 
@@ -38,9 +38,12 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 
 void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 int				ft_lstlen(t_list *lst);
+
+t_list			*ft_lstcopy(t_list *lst);
 
 #endif
